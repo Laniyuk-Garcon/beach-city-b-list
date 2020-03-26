7ab8ff42d079bb8c43a0baeaad9ed6a614a92de8
Laniyuk <laniyuk.info@gmail.com>
2020-03-27 12:25:49 +1300

    add comment form

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -38,4 +38,13 @@ router.get('/:id', (req, res) => {
 //   res.send('Character Profile goes here')
 })
 
+router.post('/:id', (req, res) =>{
+  //if (err) console.log(err)
+  res.redirect('/' + req.params.id)
+    //let obj = JSON.parse(data)
+    //let character = obj.characters.find(element => element.id == req.params.id)
+    console.log(req.body.Comment)
+})
+
+
 module.exports = router
